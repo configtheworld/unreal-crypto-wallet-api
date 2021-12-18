13acfe41c240964c18f2a4c4ff9286a1adc68858
configtheworld <erkinture@gmail.com>
2021-12-19 01:14:17 +0300

    .env cache clear

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require('express'),
   app = express(),
   createError = require('http-errors'),
   path = require('path'),
-  cookieParser = require('cookie-parser'),
   logger = require('morgan'),
   bodyParser = require('body-parser', { useNewUrlParser: true }),
   expressSanitizer = require('express-sanitizer'),
-  expressSession = require('express-session'),
   mongoose = require('mongoose'),
   passport = require('passport'),
   passportLocal = require('passport-local'),
@@ -19,6 +17,19 @@ const express = require('express'),
   wallet = require('./routes/wallet'),
   crypto = require('./routes/crypto');
 
+// Mongo Database
+/*mongoose.Promise = global.Promise;
+mongoose.connect("mongodb+srv://" +process.env.LINK+"",{
+	useNewUrlParser:true,
+	useFindAndModify:false,
+	useUnifiedTopology:true,
+	useCreateIndex:true
+}).then(()=>{
+	console.log("DB configed");
+}).catch((err)=>{
+	console.log("ERROR:",err.message);
+});
+*/
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,13 +37,25 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use(wallet);
 app.use(crypto);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
+
+app.locals.moment = require('moment');
+
+//local strategy
+passport.use(new passportLocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
