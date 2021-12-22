c86e7cda1a4bc13bc3edfb47becc52a91ea44a85
configtheworld <erkinture@gmail.com>
2021-12-22 23:49:06 +0300

    added wallet todos

--- routes/wallet.js
+++ routes/wallet.js
@@ -6,6 +6,7 @@ router.get('/wallet', function (req, res, next) {
   try {
     if (req.query.token) {
       res.send('Show wallet for user no ' + req.query.token);
+      //TODO: find by id with token and find ref wallet
     } else {
       res.send(
         'Wrong or Missing Query Try again with login token as ?token=YOUR_LOGIN_TOKEN'
@@ -26,6 +27,7 @@ router.post('/wallet', function (req, res, next) {
       res.send(
         'register for create fake wallet for user no ' + req.query.token
       );
+      //TODO: create wallet with user id with ref
     } else {
       res.send(
         'Wrong or Missing Query Try again with login token as ?token=YOUR_LOGIN_TOKEN'
@@ -48,6 +50,7 @@ router.patch('/wallet', function (req, res, next) {
         res.send(
           'Add Fake ' + req.body.coin + ' for user no ' + req.query.token
         );
+        //TODO: update wallet by id with token and select ref wallet
       } else {
         res.send(
           'In order to add token please add coin info ex:{ coin_id: "algorand", amount: 200 } to request body'
